6e92055636c03bbab323dd8a3675b8c339c245b8
Muhammad Abdul Rafay <muhammadabdulrafay73@gmail.com>
2026-08-28 14:55:21 +0500

    Update Supabase URL to remove endpoint path

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://ezkvhqtvttaaspcbsobh.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://ezkvhqtvttaaspcbsobh.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_xQhhGGm57CG8rQMaXj4lpg_WAJpSLfA";
