f0e70b7a72b2a5d89096d04149c23aa7452e8493
Muhammad Abdul Rafay <muhammadabdulrafay73@gmail.com>
2026-09-12 22:38:45 +0500

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = "https://bzfsbeoocrokgfndhykm.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://bzfsbeoocrokgfndhykm.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_iHAruYfkDZoUgdqjrjAdDw_vT8okfmp";
